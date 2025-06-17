c39d40c7d37f969e935cf71e081410ae2ad18f82
EmanNasar001 <emannasar@DESKTOP-SRB2C71.localdomain>
2025-06-17 05:13:53 +0500

    Push via SSH

--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -15,8 +15,12 @@ int main(int argc, char **argv) {
     assert(num_z == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    assert(num_occurrences("aaaaaa", 'a') == 6);
 
     printf("Congrats! If you have made it to this line, your Part 1 Test cases are all passing!\n");
+    
+
+
 
     /************ Part 2 Test Cases ************/
     DNA_sequence dna_seq_1;
@@ -36,6 +40,15 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.T_count == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    DNA_sequence dna_seq_3;
+    
+    strcpy(dna_seq_3.sequence, "TTTTGGGGCCCCAAAA");
+    compute_nucleotide_occurrences(&dna_seq_3);
+    assert(dna_seq_3.A_count == 4);
+    assert(dna_seq_3.C_count == 4);
+    assert(dna_seq_3.G_count == 4);
+    assert(dna_seq_3.T_count == 4);
+
 
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
